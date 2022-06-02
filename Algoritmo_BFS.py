e8ddd9811c87c9a737c649461ad0660b1dbbe27e
PiedraAndrick <ampiedra1@espe.edu.ec>
2022-06-02 01:22:15 -0500

    Creación de clase Grafo y su constructor

--- Algoritmo_BFS.py
+++ Algoritmo_BFS.py
@@ -1,1 +1,14 @@
 from queue import Queue
+
+class Grafo:
+     # Constructor
+    def __init__(self, numero_de_nodos, direccion=True):
+        self.numero_de_nodos= numero_de_nodos
+        self.nodos = range(self.numero_de_nodos)
+		
+        # Direccion
+        self.direccion = direccion
+		
+       # Representación gráfica - Lista de adyacencia
+        # Usamos un diccionario para implementar una lista de adyacencia
+        self.lista_adyaciencia = {nodo: set() for nodo in self.nodos} 
