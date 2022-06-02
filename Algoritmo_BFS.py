46bc1fe15b962e8d2b41652e52fd3b3e05dedc35
PiedraAndrick <ampiedra1@espe.edu.ec>
2022-06-02 01:27:56 -0500

    Función de recorrido BFS de un vertice inicial.

--- Algoritmo_BFS.py
+++ Algoritmo_BFS.py
@@ -24,3 +24,29 @@ class Grafo:
     def Mostrar_lista_adyaciencia(self):
         for llave in self.lista_adyaciencia.keys():
             print("Nodo", llave, ": ", self.lista_adyaciencia[llave])
+
+        # Función que imprime el recorrido BFS
+    # de un vértice fuente dado. bfs_transversal(int s)
+    # atraviesa vértices alcanzables desde s.
+    def recorrido(self, iniciar_nodo):
+       # Conjunto de nodos visitados para evitar bucles
+        visitado = set()
+        cola = Queue()
+
+       ## Agregue start_node a la cola y la lista visitada
+        cola.put(iniciar_nodo)
+        visitado.add(iniciar_nodo)
+
+        while not cola.empty():
+           # Desencolar un vértice hacer cola e imprimirlo
+            nodo_actual = cola.get()
+            print( nodo_actual, end= " ")
+
+            # Obtener todos los vértices adyacentes del
+            # vértice desencolado. Si un adyacente
+            # no ha sido visitado, luego márcalo
+            # visitado y ponerlo en cola
+            for (siguiente_nodo, peso) in self.lista_adyaciencia[nodo_actual]:
+                if siguiente_nodo not in visitado:
+                    cola.put(siguiente_nodo)
+                    visitado.add(siguiente_nodo)
